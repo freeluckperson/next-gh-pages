aa14d04ec6cf27056bfa406922e331b675d7fa96
freeluckperson <gestionecesaria@gmail.com>
2023-12-05 00:17:20 -0400

    5/12/2023

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,44 +1,64 @@
+"use client";
+
+import { useState } from "react";
+
 export default function RegisterPage() {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleFormSubmit(ev) {
+    ev.preventDefault();
+    fetch("/api/register", {
+      method: "POST",
+      body: JSON.stringify({ email: form.email, password: form.password }),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then(setForm({ email: "", password: "" }))
+      .catch((error) => console.log(error));
+  }
+
   return (
     <div className="">
-        <section className="mt-8 h-full flex flex-wrap justify-center ">
-            <form className="grid place-items-center" action="">
-                <h1 className="text-center text-primary text-4xl">
-                    Register
-                </h1><input className="h-10 items-center m-1 w-72 border-2 rounded-lg" type="email" name="" id="" placeholder=" Email" />
-                <input className="h-10 items-center m-1 w-72 border-2 rounded-lg" type="password" name="" id="" placeholder=" Password" />
-                <button className="h-10 items-center m-1 w-72 border-2  rounded-lg flex justify-center gap-4 bg-primary" type="submit">Register</button>
-                <div className="text-gray-400">
-                    Or login with provider
-                </div>
-                <button className="h-10 items-center m-1 w-72 border-2 rounded-lg flex justify-center gap-4">
-                    <img src="/google.png" height={24} width={24} alt="..." />
-                    Login with google
-                </button>
-            </form>
-        </section>
+      <section className="mt-8 h-full flex flex-wrap justify-center ">
+        <form
+          className="grid place-items-center"
+          action=""
+          onSubmit={handleFormSubmit}
+        >
+          <h1 className="text-center text-primary text-4xl">Register</h1>
+          <input
+            className="h-10 items-center m-1 w-72 border-2 rounded-lg"
+            type="email"
+            name=""
+            id=""
+            placeholder="email"
+            value={form.email}
+            onChange={(ev) => setForm({ ...form, email: ev.target.value })}
+          />
+          <input
+            className="h-10 items-center m-1 w-72 border-2 rounded-lg"
+            type="password"
+            name=""
+            id=""
+            placeholder="password"
+            value={form.password}
+            onChange={(ev) => setForm({ ...form, password: ev.target.value })}
+          />
+          <button
+            className="h-10 items-center m-1 w-72 border-2  rounded-lg flex justify-center gap-4 bg-primary"
+            type="submit"
+          >
+            Register
+          </button>
+          <div className="text-gray-400">Or login with provider</div>
+          <button className="h-10 items-center m-1 w-72 border-2 rounded-lg flex justify-center gap-4">
+            <img src="/google.png" height={24} width={24} alt="..." />
+            Login with google
+          </button>
+        </form>
+      </section>
     </div>
-
-    // <div className="mt-8">
-    //   <section className="">
-    //     <h1 className="text-center text-primary text-4xl">Register</h1>
-    //     <form className="block max-w-xs mx-auto" action="">
-    //       <input className="" type="email" name="" id="" placeholder=" Email" />
-    //       <input
-    //         className=""
-    //         type="password"
-    //         name=""
-    //         id=""
-    //         placeholder=" Password"
-    //       />
-    //       <button type="submit">Register</button>
-    //       <div className="">Or login with provider</div>
-    //       <button className="flex gap-4 justify-center">
-    //         <img src="/google.png" height={24} width={24} alt="..." />
-    //         Login with google
-    //       </button>
-    //     </form>
-    //   </section>
-    // </div>
   );
 }
